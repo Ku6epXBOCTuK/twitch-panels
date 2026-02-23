551f94ebe46061614e7e42ca8779637136390f5e
Ku6epXBOCTuK <Ku6epXBOCTuK@gmail.com>
2026-02-23 14:32:28 +0500

    test: fix test, after update deps web-animations does not work

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,3 +1,19 @@
 import "@testing-library/jest-dom/vitest";
 import "vitest-canvas-mock";
-import "web-animations-js";
+import { vi } from "vitest";
+
+if (typeof Element !== "undefined") {
+  Element.prototype.animate = vi.fn().mockImplementation(() => {
+    return {
+      finished: Promise.resolve(),
+      cancel: vi.fn(),
+      pause: vi.fn(),
+      play: vi.fn(),
+      reverse: vi.fn(),
+      finish: vi.fn(),
+      persist: vi.fn(),
+      currentTime: 0,
+      startTime: 0,
+    } as unknown as Animation;
+  });
+}
